027f54838caa2f3a1f92ca3a1456266e512b60c5
qaCow37 <daily.cow37@proton.me>
2026-08-08 00:18:16 +0000

    restructured prismnix home module with better architecture

--- homeModules/prismnix/default.nix
+++ homeModules/prismnix/default.nix
@@ -1,7 +1,28 @@
-args:
-{	
+{inputs, lib, ...}@args: {config, pkgs, ...}:
+{
 	imports = [
-		(import ./options.nix args)
-		(import ./prism.nix   args)
+		(import ./instance.nix args)
 	];
+
+	options.programs.prismnix = {
+		enable = lib.mkEnableOption "Enable prismnix";
+
+		package = lib.mkOption {
+			type = lib.types.nullOr lib.types.package;
+			default = inputs.prismlauncher.packages.${pkgs.stdenv.hostPlatform.system}.default;
+			description = "Prismlauncher package to install";
+		};
+
+		path = lib.mkOption {
+			type = lib.types.str;
+			default = "${config.xdg.dataHome}/PrismLauncher";
+			description = "Path to prismlauncher directory";
+		};
+	};
+	config = lib.mkIf config.programs.prismnix.enable {
+		home.packages = let pkg = config.programs.prismnix.package; in
+			lib.mkIf (pkg != null) [
+				pkg
+			];
+	};
 }
--- homeModules/prismnix/instance.nix
+++ homeModules/prismnix/instance.nix
@@ -1,68 +1,148 @@
-{prism-lib, ...}: {lib, pkgs, ...}:
-{
-	build = name: dst: cfg:
-	let
-		resource = let rsc = cfg.resources; types = prism-lib.types.resource; in
-		{
-			activations = builtins.filter (v: v != null) (map (item:
-				if types.any-activation.check item
-					then types.call-activation
-						item
-						"${dst}/minecraft"
-					else null
-			) rsc);
+{lib, ...}: {config, pkgs, ...}:
+let
+	instance-path = name: "${config.programs.prismnix.path}/instances/${name}";
 
-			packages = builtins.filter (item:
-				types.package.check
-					item
-			) rsc;
-		};
-	in
+	instance = {name, ...}:
 	{
-		activation =
-		let
-			# transform list of activation scripts to list of activation scripts
-			# with the name of the activation script prefixed with instance name
-			resources = lib.mergeAttrsList (map (item:
-				{
-					"prismlauncher-nix.${name}.${item.name}" = item.entry;
-				}
-			) resource.activations);
-		in
-		{
-			"prismlauncher-nix.${name}" =
-			let
-				mmc-pack = pkgs.writeText "prismlauncher-nix.${name}.mmc-pack" (
-					lib.toJSON {
-						components = cfg.components;
-						formatVersion = 1;
-					}
-				);
-				crudini = "${lib.getExe pkgs.crudini}";
-			in lib.hm.dag.entryAfter ["linkGeneration"]
-			''
-				mkdir -p "${dst}"
-				cp -f "${mmc-pack}" "${dst}/mmc-pack.json"
-				touch "${dst}/instance.cfg"
-
-				${crudini} --set "${dst}/instance.cfg" "General" "InstanceType" "OneSix"
-				${crudini} --set "${dst}/instance.cfg" "General" "ConfigVersion" "1.3"
-				${crudini} --set "${dst}/instance.cfg" "General" "name" "${cfg.config.name}"
-			'';
-		}
-		// resources;
-
-		file."${dst}/minecraft/" =
-		let
-			pkg = pkgs.callPackage prism-lib.pkgs.mkPackage {
-				name = "${name}-mcpack";
-				deps = resource.packages;
+		options = {
+			path = lib.mkOption {
+				type = lib.types.str;
+				readOnly = true;
+				description = "The path to install the instance to";
 			};
-		in
-		lib.mkIf ((builtins.length resource.packages) > 0)
-		{
-			source = "${pkg}/minecraft/";
-			recursive = true;
+
+			instance = {
+				config = lib.mkOption {
+					type = lib.types.attrsOf (lib.types.submodule ({name, ...}:{
+						options = {
+							value = lib.mkOption {
+								type = lib.types.str;
+								description = "The value of the option";
+							};
+							force = lib.mkOption {
+								type = lib.types.bool;
+								default = false;
+								description = "If the option should be overriden when it already exists";
+							};
+							target = lib.mkOption {
+								type = lib.types.str;
+								default = name;
+								description = "The target option name";
+							};
+						};
+					}));
+					description = "All options to be written to the instance.cfg file";
+				};
+				components = lib.mkOption {
+					type = lib.types.listOf lib.prismnix.types.jsonobj;
+					default = [];
+					description = "The prismlauncher instance components";
+				};
+				packages = lib.mkOption {
+					type = lib.types.listOf lib.types.package;
+					default = [];
+					description = "Packages to install into the instance";
+				};
+				file = lib.mkOption {
+					type = lib.types.attrsOf lib.prismnix.types.file;
+					default = {};
+					description = "Files to create in the instance";
+				};
+				activation = lib.mkOption {
+					type = lib.hm.types.dagOf lib.types.str;
+					default = {};
+					description = "The activation scripts to run for the instance";
+				};
+			};
 		};
+		config.path = "${instance-path name}/minecraft/";
 	};
+
+	option-entries = builtins.attrNames (builtins.readDir ./options);
+	options = map(item:
+		./options + "/${item}"
+	) option-entries;
+in
+{
+	options.programs.prismnix.instances = lib.mkOption {
+		type = lib.types.attrsOf (lib.types.submoduleWith {
+			modules = [instance] ++ options;
+			specialArgs = {
+				lib = lib;
+			};
+			shorthandOnlyDefinesConfig = true;
+		});
+		default = {};
+		description = "All instances to create for prismlauncher";
+	};
+
+	config.home.activation = lib.mkIf config.programs.prismnix.enable (
+		lib.mkMerge (lib.mapAttrsToList (name: instance:
+			let
+				cfg = instance.instance;
+				path = instance-path name;
+
+				mmc-pack = pkgs.writeText "mmc-pack.json" (lib.toJSON {
+					components = cfg.components;
+					formatVersion = 1;
+				});
+				set-cfg = opt: let
+					cmd = lib.getExe pkgs.crudini;
+					cfg = "${path}/instance.cfg";
+					get = ''${cmd} --get "${cfg}" General "${opt.target}"'';
+					set = ''${cmd} --set "${cfg}" General "${opt.target}" "${opt.value}"'';
+				in if !opt.force
+					then ''
+						if ${get} >/dev/null 2>&1; then
+							run ${set}
+						fi
+					''
+					else ''run ${set}'';
+
+				initial-activation = {
+					"prismnix.${name}" = lib.hm.dag.entryAfter ["linkGeneration"]
+					''
+						run mkdir -p "${path}"
+						run cp -f ${mmc-pack} "${path}/mmc-pack.json"
+
+						${lib.concatMapAttrsStringSep
+							"\n"
+							(_: v: set-cfg v)
+							cfg.config
+						}
+					'';
+				};
+			in cfg.activation // initial-activation
+		) config.programs.prismnix.instances)
+	);
+
+	config.home.file = lib.mkIf config.programs.prismnix.enable (
+		lib.mkMerge (lib.mapAttrsToList (name: instance:
+			let
+				cfg = instance.instance;
+				file = lib.mapAttrs' (filename: file: {
+					name = "${instance.path}/${filename}";
+					value = {
+						enable = file.enable;
+						text = file.text;
+						force = file.force;
+						source = lib.mkIf (file ? source) file.source;
+						target = "${instance.path}/${filename}";
+						onChange = file.onChange;
+						recursive = file.recursive;
+					};
+				}) cfg.file;
+
+				packages = lib.listToAttrs (map (item: {
+					name = "${instance.path}/${item}";
+					value = {
+						force = true;
+						recursive = true;
+						source = "${item}/minecraft/";
+						target = "${instance.path}";
+					};
+				}) (lib.unique cfg.packages));
+			in file // packages
+		) config.programs.prismnix.instances)
+	);
 }
