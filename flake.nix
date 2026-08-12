c0dea99fad54c975cd028468c072549d39e5de90
qaCow37 <daily.cow37@proton.me>
2026-08-12 19:02:49 +0000

    added packages and overlay to flake

--- flake.nix
+++ flake.nix
@@ -19,17 +19,28 @@
 		homeModules = {
 			prismnix = import ./homeModules/prismnix
 				{
+					self = self;
 					lib = lib;
 					inputs = inputs;
 				};
 		};
 		components   = import ./components {inherit lib;};
 		lib.prismnix = import ./lib        {inherit lib;};
+
+		overlays = {
+			default = final: prev: {
+				prismnix = self.packages.${prev.stdenv.hostPlatform.system};
+			};
+		};
 	} // flake-utils.lib.eachDefaultSystem (system:
 		let
 			pkgs = nixpkgs.legacyPackages.${system};
 		in
 		{
+			packages = import ./packages {
+				lib = lib;
+				callPackage = pkgs.callPackage;
+			};
 			devShells.default = pkgs.mkShell {
 				packages = with pkgs; [
 					python3
@@ -37,6 +48,7 @@
 					python3Packages.packaging
 					python3Packages.requests-ratelimiter
 					python3Packages.typer
+					python3Packages.jinja2
 				];
 			};
 		}
