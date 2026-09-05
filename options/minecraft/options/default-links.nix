4323921c78151623dacd45adc469e9cf85624dd6
qaCow37 <daily.cow37@proton.me>
2026-09-05 18:52:38 +0000

    added default-links suboptions

--- options/minecraft/options/default-links.nix
+++ options/minecraft/options/default-links.nix
@@ -13,14 +13,44 @@
                 " - /shaderpacks"
             ];
         };
+        mods = lib.mkOption {
+            type = lib.types.bool;
+            example = false;
+            default = config.default-links.enable;
+            defaultText = "<default-links.enable>";
+            description = "Whether to use the default mod directory symlink";
+        };
+        resourcepacks = lib.mkOption {
+            type = lib.types.bool;
+            example = false;
+            default = config.default-links.enable;
+            defaultText = "<default-links.enable>";
+            description = "Whether to use the default resourcepack directory symlink";
+        };
+        shaderpacks = lib.mkOption {
+            type = lib.types.bool;
+            example = false;
+            default = config.default-links.enable;
+            defaultText = "<default-links.enable>";
+            description = "Whether to use the default shaderpack directory symlink";
+        };
     };
     config.instance = lib.mkIf config.default-links.enable {
         filesystem = lib.prismnix.mkDir {
-            minecraft = lib.prismnix.mkDir {
-                "mods"          = lib.prismnix.mkDrvLink {};
-                "resourcepacks" = lib.prismnix.mkDrvLink {};
-                "shaderpacks"   = lib.prismnix.mkDrvLink {};
-            };
+            minecraft = lib.prismnix.mkDir (
+                (if config.default-links.mods
+                    then {"mods" = lib.prismnix.mkDrvLink {};}
+                    else {}
+                ) //
+                (if config.default-links.resourcepacks
+                    then {"resourcepacks" = lib.prismnix.mkDrvLink {};}
+                    else {}
+                )//
+                (if config.default-links.shaderpacks
+                    then {"shaderpacks" = lib.prismnix.mkDrvLink {};}
+                    else {}
+                )
+            );
         };
     };
 }
